80b7a7efea49c7864dfa208163618ecb041eabca
Andriy Borysov <andriy.borysov@ukr.net>
2019-06-25 21:27:03 +0300

    Function to power a number is implemented.

--- src/com/aborysov/ubiquitous_enigma/Main.java
+++ src/com/aborysov/ubiquitous_enigma/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static int pow(int value, int level) {
+        return (int) Math.pow(value, level);
+    }
 }
